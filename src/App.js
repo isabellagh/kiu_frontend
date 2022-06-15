90534630c6d81a03b71bace2ef115932dd36bf53
isabellagh <isabellagh88@gmail.com>
2022-06-15 15:01:22 -0400

    redux store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
+  componentDidMount() {
+    fetch('http://[::1]:3000/api/v1/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   render() {
     return <div className='App'>App</div>;
   }
